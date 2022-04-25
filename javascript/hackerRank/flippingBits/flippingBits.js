7b81e3709dea65a80fe0cb54aa8d1f5d8b4e8d8f
Ayrat <gimranov45@gmail.com>
2022-04-25 01:50:23 -0700

    finished flippingBits

--- javascript/hackerRank/flippingBits/flippingBits.js
+++ javascript/hackerRank/flippingBits/flippingBits.js
@@ -1,18 +1,25 @@
 'use strict';
 
 function flippingBits(n) {
-  let arr = [];
+  let flippedBinary = [];
   const _divideByTwo = (int) => {
-    arr.push(int % 2 ? 1 : 0)
+    flippedBinary.push(int % 2 ? 0 : 1)
     let half = Math.floor(int / 2);
     if (half === 0) return
     else _divideByTwo(half)
   }
-
   _divideByTwo(n);
-  return arr
+
+  let pad = new Array(32 - flippedBinary.length).fill(1);
+  flippedBinary.push(...pad);
+  let result = 0;
+  for (let i = 0; i < flippedBinary.length; i++){
+    let val = flippedBinary[i] * (2 ** i);
+    result += val;
+  }
+  return result
 }
 
-console.log(flippingBits(9));
+console.log(flippingBits(0));
 
 
